55c79d46465f43494e2390e48d787dbb8a72273e
Akhmad Herdian <akhmad.herdian@gmail.com>
2019-07-02 05:07:27 +0800

    Create outlet basic crud

---
+++ resources/lang/id/outlet.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    // Labels
+    'outlet'     => 'Outlet',
+    'list'           => 'Daftar Outlet',
+    'search'         => 'Cari Outlet',
+    'search_text'    => 'Nama ...',
+    'all'            => 'Semua Outlet',
+    'select'         => 'Pilih Outlet',
+    'detail'         => 'Detail Outlet',
+    'not_found'      => 'Outlet tidak ditemukan',
+    'empty'          => 'Belum ada Outlet',
+    'back_to_show'   => 'Kembali ke detail Outlet',
+    'back_to_index'  => 'Kembali ke daftar Outlet',
+
+    // Actions
+    'create'         => 'Input Outlet Baru',
+    'created'        => 'Input Outlet baru telah berhasil.',
+    'show'           => 'Lihat Detail Outlet',
+    'edit'           => 'Edit Outlet',
+    'update'         => 'Update Outlet',
+    'updated'        => 'Update data Outlet telah berhasil.',
+    'delete'         => 'Hapus Outlet',
+    'delete_confirm' => 'Anda yakin akan menghapus Outlet ini?',
+    'deleted'        => 'Hapus data Outlet telah berhasil.',
+    'undeleted'      => 'Data Outlet gagal dihapus.',
+    'undeleteable'   => 'Data Outlet tidak dapat dihapus.',
+
+    // Attributes
+    'name'           => 'Nama Outlet',
+    'description'    => 'Deskripsi Outlet',
+];
